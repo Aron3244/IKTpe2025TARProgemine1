bccaaa7e0405f1ba3cedd714a635ffdfaf17e42f
Opilane TTHK <opilane@tthk.ee>
2026-02-03 15:12:52 +0200

    Dictinary

---
+++ 01.03.02.26/Dictinary/Dictinary/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Dictinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictinary ja foreach");
+
+            //var on muutuja
+            var domains = new Dictionary<string, string>()
+            {
+                { "fi", "Finland" },
+                { "se", "Sweden" },
+                { "de", "Germany" },
+                { "fr", "France" },
+                { "es", "Spain" }
+            };
+            //kasutage foreachi ja juurde tuleb lisada komas rida
+            int i = 1;
+            foreach (var domain in domains)
+            {
+                Console.WriteLine($"{domain.Key} - {domain.Value} - {i}");
+                i++;
+            }
+           
+        }
+    }
+}
