4030628ec3de4407007d28239fcaf4c135353143
Opilane TTHK <opilane@tthk.ee>
2026-02-06 09:58:42 +0200

    RegEx

---
+++ 02.06.02.26/RegEx/RegEx/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teeme Regular Exressio harjutuse");
+
+            string word = "#CD5C5CA";
+            Console.WriteLine("Hex code: " + word);
+            Console.WriteLine("Kas on regex: " + RegExTest(word));
+        }
+
+        public static bool RegExTest(string word)
+        {
+            //Regular Expressin kontrollib, kas sisestav string vastab nõuetele
+            return System.Text.RegularExpressions.Regex.IsMatch(word, @"[#][0-9A-Fa-f]{6}\b");
+        }
+    }
+}
