f1357abb91112175cdf449f78167b6d2753f9f79
Opilane TTHK <opilane@tthk.ee>
2026-02-17 15:53:54 +0200

    Database

--- 04.17.02.26/LINKQ/LINKQ/Program.cs
+++ 04.17.02.26/LINKQ/LINKQ/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using LINKQ;
+using LINKQ.Models;
+
+namespace LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Kutsume esile LINQ läbi swichi!");
+            Console.WriteLine("Valik vastav link numbriga");
+            Console.WriteLine("1. Where");
+
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    WhereLINQ();
+                    break;
+
+                case 2:
+                    WhereByNameLINQ();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        //teeme uue meetodi
+
+        public static void WhereLINQ()
+        {
+            var peopleAge = PeopleData.peoples
+                .Where(x => x.Age > 20 && x.Age < 23);
+
+            //kasutada muutuja peopleAge ja kuvab andmed esile
+            //kasutada foreachi
+
+            foreach (var person in peopleAge)
+            {
+                Console.WriteLine(person.Name);
+
+            }
+        }
+        public static void WhereByNameLINQ()
+        {
+            Console.WriteLine("Kirjuta inimese nimi: ");
+            string name = Console.ReadLine();
+
+            //kasutada where imimese otsimiseks
+            //otsimine toimub nime alusel
+            var people = PeopleData.peoples
+            .Where(x => x.Name == name);
+
+            foreach (var people in peopleData)
+            {
+                Console.WriteLine(people.Name + " " + people.Age );
+            }
         }
     }
 }
