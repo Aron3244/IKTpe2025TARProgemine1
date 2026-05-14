e131a63f7c414a09e2072cfe4ab4d57d9f7f75af
Opilane TTHK <opilane@tthk.ee>
2026-05-14 14:56:02 +0300

    CourseAssignment

--- University/University/University/Models/Course.cs
+++ University/University/University/Models/Course.cs
@@ -8,7 +8,11 @@ namespace University.Models
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
+        public int DeapartmenId { get; set; }
+        public Deapartment Deapartments { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
 }
---
+++ University/University/University/Models/OfficeAssingment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace University.Models
+{
+    public class OfficeAssignment
+    {
+        //kui soovite konkreetset välja tuua, et InstructorId on nii OfficeAssignmenti
+        //peamine võti kui ka võõrvõti, siis saate kasutada [Key] ja [ForeingKey]atribuute:
+        [Key]
+        public int InstructorId { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+
+        public Instructor Instructor { get; set; }
+    }
+}
