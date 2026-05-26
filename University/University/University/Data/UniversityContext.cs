9cf0834af0a7230a210424a3cd44d2c2d82e24cc
Opilane TTHK <opilane@tthk.ee>
2026-05-26 14:56:06 +0300

    New Migratsion

--- University/University/University/Data/UniversityContext.cs
+++ University/University/University/Data/UniversityContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using University.Models;
+using University.ViewModel;
 
 namespace University.Data
 {
@@ -11,12 +12,24 @@ namespace University.Data
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Deapartment> Deapartments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
             modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Deapartment>().ToTable("Deapartment");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseId, c.InstructorId });
+
         }
     }
 }
---
+++ University/University/University/Migrations/20260526115108_TabelAdd.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DeapartmenId",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeapartmentsDeapartmentId",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Instructor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Instructor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CourseAssignment",
+                columns: table => new
+                {
+                    InstructorId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseAssignment", x => new { x.CourseId, x.InstructorId });
+                    table.ForeignKey(
+                        name: "FK_CourseAssignment_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseAssignment_Instructor_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deapartment",
+                columns: table => new
+                {
+                    DeapartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Budget = table.Column<decimal>(type: "money", nullable: false),
+                    StarDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InstructorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deapartment", x => x.DeapartmentId);
+                    table.ForeignKey(
+                        name: "FK_Deapartment_Instructor_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructor",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfficeAssignment",
+                columns: table => new
+                {
+                    InstructorId = table.Column<int>(type: "int", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficeAssignment", x => x.InstructorId);
+                    table.ForeignKey(
+                        name: "FK_OfficeAssignment_Instructor_InstructorId",
+                        column: x => x.InstructorId,
+                        principalTable: "Instructor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_DeapartmentsDeapartmentId",
+                table: "Course",
+                column: "DeapartmentsDeapartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseAssignment_InstructorId",
+                table: "CourseAssignment",
+                column: "InstructorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deapartment_InstructorId",
+                table: "Deapartment",
+                column: "InstructorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Deapartment_DeapartmentsDeapartmentId",
+                table: "Course",
+                column: "DeapartmentsDeapartmentId",
+                principalTable: "Deapartment",
+                principalColumn: "DeapartmentId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Deapartment_DeapartmentsDeapartmentId",
+                table: "Course");
+
+            migrationBuilder.DropTable(
+                name: "CourseAssignment");
+
+            migrationBuilder.DropTable(
+                name: "Deapartment");
+
+            migrationBuilder.DropTable(
+                name: "OfficeAssignment");
+
+            migrationBuilder.DropTable(
+                name: "Instructor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_DeapartmentsDeapartmentId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "DeapartmenId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "DeapartmentsDeapartmentId",
+                table: "Course");
+        }
+    }
+}
