242374113cae835f614bc362c2c323818e633b12
Opilane TTHK <opilane@tthk.ee>
2026-05-28 12:03:20 +0300

    Update Course

--- University/University/Controllers/CourseController.cs
+++ University/University/Controllers/CourseController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using University.Data;
+using University.Models;
 using University.ViewModel;
+using University.ViewModel.CoursesVM;
 
 namespace University.Controllers
 {
@@ -36,6 +38,54 @@ namespace University.Controllers
 
             return View(course);
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.CourseId == id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new CourseUpdateViewModel
+            {
+                CourseId = course.CourseId,
+                Title = course.Title,
+                Credits = course.Credits,
+                DepartmentId = course.DepartmentId
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+  
+        public async Task<IActionResult> Update(CourseUpdateViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = new Models.Course
+                {
+                    CourseId = vm.CourseId,
+                    Title = vm.Title,
+                    Credits = vm.Credits,
+                    DepartmentId = vm.DepartmentId
+                };
+
+                var courseUpdateId = course.CourseId;
+
+                _context.Update(course);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Update), new { id = courseUpdateId });
+            }
+
+            return View(vm);
         }
     }
 }
